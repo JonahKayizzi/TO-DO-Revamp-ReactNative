216a879431402bd04aa2d1b90908b6b37c0911bd
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-06-23 23:35:09 +0300

    Add styling for header

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,20 +1,40 @@
 /* eslint-disable no-use-before-define */
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 const profilePic = require('../images/PROFFESSION.jpg');
 
 const AppHeader = () => (
-  <View>
-    <View>
+  <View style={styles.appheader}>
+    <View style={styles.profile}>
       <Image source={profilePic} />
-      <Text>Good evening, Jonathan</Text>
+      <Text style={styles.greeting}>Good evening, Jonathan</Text>
     </View>
-    <View>
-      <Text>22 JUN, 2023</Text>
-      <Text>16:10</Text>
+    <View style={styles.datetime}>
+      <Text style={styles.date}>22 JUN, 2023</Text>
+      <Text style={styles.time}>16:10</Text>
     </View>
   </View>
 );
 
 export default AppHeader;
+const styles = StyleSheet.create({
+  appheader: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    paddingHorizontal: 20,
+    paddingVertical: 4,
+    boxShadow: '0 1px 6px 0 rgba(0, 0, 0, 0.38)',
+  },
+  profilePic: {
+    width: '50px',
+    height: '50px',
+  },
+  profile: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+});
